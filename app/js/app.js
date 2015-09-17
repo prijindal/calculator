f070dd3c4d7fc4a9b92ca50a2754f4bca012cf09
prijindal <priyanshujindal1995@gmail.com>
2015-09-18 03:46:45 +0530

    Basic animations added

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
 angular.module('calculator', [])
 .controller('mainController',
-['ipc', 'expression',
-function(ipc, expression) {
+['$scope','ipc', 'expression',
+function($scope,ipc, express) {
     var self = this;
-    var expression = expression.exp
+    var expression = express.exp
 
     self.close = ipc.closeWindow
     self.expression = expression.list
@@ -20,12 +20,48 @@ function(ipc, expression) {
             expression.push(char.toString())
         }
     }
+    var toggle = 1;
     self.eval = function() {
+        var input1 = angular.element('#result2')
+        var input2 = angular.element('#result1')
+        if (toggle) {
+            input1 = angular.element('#result1')
+            input2 = angular.element('#result2')
+        }
+        toggle = !toggle
+        var time = 500
+        var height = input1.css('height')
+
         var evaluated = eval(expression.list.join(''))
-        expression.clear()
-        expression.push(evaluated)
+        input1.slideUp(time, function() {
+            expression.clear()
+        })
+        input2.css('height', 0)
+        input2.css('margin-top',height)
+        input2.css('display','inline-flex')
+        input2.css('font-size','0')
+        input2.children('input').after('<span class="sample">'+evaluated+'</span>')
+        input2.animate({
+            height:height,
+            'margin-top':0
+        }, time, function(){
+            input2.css('font-size','20px')
+            expression.push(evaluated)
+            input2.children('.sample').remove()
+            $scope.$digest()
+        })
     }
-    self.clear = expression.clear
+    self.clear = function() {
+        expression.clear()
+    }
+
+    angular.element('body').on('keyup', function(e) {
+        var key = e.keyCode
+        if (key>=48&&key<=57) {
+            self.add(key-48)
+            $scope.$digest()
+        }
+    })
 
     var isDigit = function(data) {
         return data === parseInt(data, 10)
